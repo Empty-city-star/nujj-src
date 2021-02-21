7bd26b5ed97fa2c14a07f5ee305e3f4511ed7b57
夜里无星 <1042633805@qq.com>
2021-02-22 05:36:22 +0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,13 +1,21 @@
-# This is purely the result of trial and error.
-
-import sys
-
-class PyTest():
-    """
-    Running `$ python setup.py test' simply installs minimal requirements
-    and runs the tests with no fancy stuff like parallel execution.
-
-    """
-    def run_tests(self):
-        import pytest
-        sys.exit(pytest.main(self.test_args))
+# -*- coding: UTF-8 -*-
+while True:
+ I = input("pls input the lirun:")
+ if I <= 10:
+  a = I * 0.01
+  print a
+ elif I <= 20 and I > 10:
+  b =0.25 + I * 0.075
+  print b
+ elif I <= 40 and I > 20:
+  c = 0.75 + I * 0.05
+  print c
+ elif I <= 60 and I > 40:
+  d = 0.95 + I * 0.03
+  print d
+ elif I <= 60 and I > 100:
+  e = 2 + I * 0.015
+  print e
+ else:
+  f = 2.95 + I * 0.01
+  print f
